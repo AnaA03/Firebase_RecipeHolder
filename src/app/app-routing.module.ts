6cfc85185cf0944316f3de207a5ae9d78d14d3ea
Anagha Deshpande <anagha.ambekar03@gmail.com>
2025-03-19 16:10:45 +0530

    Added bootstrap and logo Component done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogoComponentComponent } from './pages/logo-component/logo-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home',component:LogoComponentComponent}, 
+  {path: '',component:LogoComponentComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
